bd582417587ed98cea51090125ed898f6ed23812
DeepanAndroid <mailtodeebanmca@gmail.com>
2019-06-06 09:28:07 +0530

    Live crash issue fix

--- app/src/main/java/online/motohub/util/ConnectivityChangeReceiver.java
+++ app/src/main/java/online/motohub/util/ConnectivityChangeReceiver.java
@@ -3,23 +3,25 @@ package online.motohub.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
-import online.motohub.activity.BaseActivity;
 import online.motohub.database.DatabaseHandler;
 import online.motohub.model.SpectatorLiveEntity;
 
-public class ConnectivityChangeReceiver  extends BroadcastReceiver {
+public class ConnectivityChangeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().equalsIgnoreCase("android.net.conn.CONNECTIVITY_CHANGE")) {
-            Log.e("CONNECTIVITY_STATUS -->","TRUE");
-            if(((BaseActivity)context).isNetworkConnected(context))
-                Log.e("CONNECTIVITY -->","TRUE");
+        if (intent.getAction().equalsIgnoreCase("android.net.conn.CONNECTIVITY_CHANGE")) {
+            Log.e("CONNECTIVITY_STATUS -->", "TRUE");
+            boolean isLoggedIn = PreferenceUtils.getInstance(context).getBooleanData(PreferenceUtils.USER_KEEP_LOGGED_IN);
+            if (isLoggedIn && isNetworkConnected(context))
+                Log.e("CONNECTIVITY -->", "TRUE");
             uploadOffline(context);
         }
     }
@@ -28,12 +30,25 @@ public class ConnectivityChangeReceiver  extends BroadcastReceiver {
         DatabaseHandler handler = new DatabaseHandler(context);
         ArrayList<SpectatorLiveEntity> mList = handler.getSpectatorLiveVideos();
         if (mList.size() > 0) {
-                for (int i = 0; i < mList.size(); i++) {
-                    Intent service_intent = new Intent(context, UploadOfflineVideos.class);
-                    String data = new Gson().toJson(mList.get(i));
-                    service_intent.putExtra("data", data);
-                    context.startService(service_intent);
-                }
+            for (int i = 0; i < mList.size(); i++) {
+                Intent service_intent = new Intent(context, UploadOfflineVideos.class);
+                String data = new Gson().toJson(mList.get(i));
+                service_intent.putExtra("data", data);
+                context.startService(service_intent);
+            }
         }
+    }
+
+    private boolean isNetworkConnected(Context context) {
+        ConnectivityManager conMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        assert conMgr != null;
+        if (conMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED
+                || conMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+
+            return true;
+        } else
+            return conMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() != NetworkInfo.State.DISCONNECTED
+                    && conMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() != NetworkInfo.State.DISCONNECTED;
     }
 }
